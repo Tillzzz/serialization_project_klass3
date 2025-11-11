4e2eee9b7987847c28634c8b8898923105e2001d
Dmytro <zigzag.till@gmail.com>
2025-11-11 22:11:22 +0200

    pydantic_2

--- pydantic_2.py
+++ pydantic_2.py
@@ -1,1 +1,16 @@
-print(111)
+from typing import List
+
+from pydantic import BaseModel
+
+class Person(BaseModel):
+    name: str
+    age: int
+
+person = Person(name='John', age=20)
+js = person.model_dump_json(indent=2)
+print(js)
+
+with open('person.json', 'w') as f:
+    f.write(js)
+
+
